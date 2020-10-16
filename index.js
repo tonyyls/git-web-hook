d37d41e472859f56c1fcfbe5926ef81b714c91da
yulsh <570519827@qq.com>
2020-10-16 11:31:21 +0800

    支持校验secret token

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-'use strict';
-const fs = require('fs-extra');
-const path = require('path');
-const http = require('http');
-const qs = require('query-string');
-const logger = require('./log.js');
-const git = require('./git.js');
-const config = require('./config.json');
+"use strict";
+const fs = require("fs-extra");
+const path = require("path");
+const http = require("http");
+const qs = require("query-string");
+const logger = require("./log.js");
+const git = require("./git.js");
+const config = require("./config.json");
 
 const port = config.port;
 const localRepo = path.join(config.localRepo);
+const secret = config.secret;
 
 // 确保拉取代码的目录存在
 function ensureLocalRepo() {
@@ -23,7 +24,7 @@ function handleRequest(req, res) {
   // 考虑url后面带参数来支持更多特性
   const url = req.url;
   let queryOptions, contextPath;
-  let index = url.indexOf('?');
+  let index = url.indexOf("?");
   if (index > 0) {
     queryOptions = qs.parse(url.substring(index + 1));
     contextPath = url.substring(0, index);
@@ -31,15 +32,21 @@ function handleRequest(req, res) {
     queryOptions = {};
     contextPath = url;
   }
+  let isValid = checkReqValid(req);
+  if (!isValid) {
+    res.writeHead(401, { "Content-Type": "text/plain" });
+    res.end("Secret token is invalid.");
+    return;
+  }
 
-  if (req.method == 'POST') {
-    let data = '';
-    req.on('data', chunk => {
+  if (req.method == "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
       data += chunk;
     });
-    req.on('end', async () => {
-      // 当请求结束时候，立刻回复服务器
-      res.write('ok');
+    req.on("end", async () => {
+      // 当请求结束时候，立刻回复git服务器
+      res.write("ok");
       res.end();
 
       data = decodeURI(data);
@@ -51,18 +58,28 @@ function handleRequest(req, res) {
         await fetchRepo(repo, queryOptions);
         await copyRepo(repo, queryOptions, contextPath);
         await execScript(repo);
-        logger.info('Successfull.');
+        logger.info("Successful.");
       }
     });
   }
+}
+
+// 检查请求是否合法：校验secret
+function checkReqValid(req) {
+  let headers = req.headers;
+  let token = headers["x-gitlab-token"];
+  if (token) {
+    return token === secret;
+  }
+  return true;
 }
 
 // 获取仓库在本地的地址
 function getRepoLocalPath(repo) {
   const url = repo.url;
   const repoName = url.substring(
-    url.lastIndexOf('/') + 1,
-    url.lastIndexOf('.')
+    url.lastIndexOf("/") + 1,
+    url.lastIndexOf(".")
   );
   const repoPath = path.join(localRepo, repoName);
   return repoPath;
@@ -72,23 +89,23 @@ function getRepoLocalPath(repo) {
 async function fetchRepo(repo, options) {
   const url = repo.url;
   const repoPath = getRepoLocalPath(repo);
-  const branch = options.branch || repo.branch || '';
+  const branch = options.branch || repo.branch || "";
   if (fs.existsSync(repoPath)) {
     return await git
       .pull(repoPath)
       .then(() => {
-        logger.info('git pull finish');
+        logger.info("git pull finish");
       })
-      .catch(error => {
+      .catch((error) => {
         logger.error(error);
       });
   } else {
     return await git
       .clone(url, branch, localRepo)
       .then(() => {
-        logger.info('git clone finish');
+        logger.info("git clone finish");
       })
-      .catch(error => {
+      .catch((error) => {
         logger.error(error);
       });
   }
@@ -96,13 +113,14 @@ async function fetchRepo(repo, options) {
 
 // 从配置中找到对应的仓库
 function findRepository(repo) {
-  let urls = [];
-  urls.push(repo['ssh_url']);
-  urls.push(repo['clone_url']);
-  urls.push(repo['git_ssh_url']);
-  urls.push(repo['git_http_url']);
+  let urls = [
+    repo["ssh_url"],
+    repo["clone_url"],
+    repo["git_ssh_url"],
+    repo["git_http_url"],
+  ];
   let result;
-  config.repository.map(item => {
+  config.repository.map((item) => {
     if (urls.includes(item.url)) {
       result = item;
       return;
@@ -113,7 +131,7 @@ function findRepository(repo) {
 
 // 拷贝仓库到指定的工作路径
 async function copyRepo(repo, options, contextPath) {
-  const dir = options.dir || '';
+  const dir = options.dir || "";
   let repoPath = getRepoLocalPath(repo);
   // 拷贝指定dir
   repoPath = path.join(repoPath, dir);
@@ -131,7 +149,7 @@ async function copyRepo(repo, options, contextPath) {
 
 // 拷贝完成后将会在对应的目录下执行的脚本
 async function execScript(repo) {
-  // todo
+  // todo 限制在某个目录执行脚本，否则很大安全隐患
 }
 
 const server = http.createServer(handleRequest);
